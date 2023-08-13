0e80c25fa4c042a4af279c42573a035b4aec8659
Rafael <rafaelrocho_@hotmail.com>
2023-08-13 00:14:29 -0300

    v0.2 adicionado alguns art(162-165)

--- actions/actions.py
+++ actions/actions.py
@@ -2,6 +2,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+
+# Informações sobre multas e gravidade
 class ActionMultaGravidade(Action):
     def name(self) -> Text:
         return "action_multa_gravidade"
@@ -9,34 +11,59 @@ class ActionMultaGravidade(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        message = "Segue abaixo as informações sobre as infrações:\n\n"
-        message += "Infração leve: R$ 88,38 - pontos na CNH: 3\n"
-        message += "Infração média: R$ 130,16 - pontos na CNH: 4\n"
-        message += "Infração grave: R$ 195,23 - pontos na CNH: 5\n"
-        message += "Infração gravíssima: R$ 293,47 - pontos na CNH: 7\n"
-        message += "OBS: Infração gravíssima possuem o fator multiplicador variando de acordo com a infração cometida, podendo ir de x2 até x20 o valor\n"
-
-        dispatcher.utter_message(text=message)
+        
+        dispatcher.utter_message(text="utter_multa_gravidade")
         return []
 
+# informações sobre pontuação na CNH
 class ActionPontuacaoCNH(Action):
     def name(self) -> Text:
-        return "action_pontuacao_cnh"
+        return "action_pontuação_cnh"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="A CNH possui um limite de 20 pontos. Quando o condutor atinge esse limite, sua habilitação é suspensa.")
+     # Enviando a resposta previamente definida no arquivo responses.md
+        dispatcher.utter_message(text="utter_pontuação_cnh")
         return []
+
+# Artigos
+
+class ActionArt162ao164(Action):
+    def name(self) -> Text:
+        return "action_art_162_ao_164"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(response="utter_art_162_ao_164")
+        return []
+
+class ActionArt165AaoC(Action):
+    def name(self) -> Text:
+        return "action_art_165_A_ao_C"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="utter_art_165_A_ao_C")
+        return []
+
+
+
 
 
 # Resposta quando nenhuma intenção específica é identificada
-class ActionDefaultFallback(Action):
-    def name(self) -> Text:
-        return "action_default_fallback"
+# class ActionDefaultFallback(Action):
+#     def name(self) -> Text:
+#         return "action_default_fallback"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_default")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(template="utter_default")
+#         return []
+
+
--- data/responses.md
+++ data/responses.md
@@ -1,2 +1,13 @@
 ## utter_default
 - Desculpe, não entendi o que você disse. Pode reformular sua pergunta?
+
+## utter_pontuação_cnh
+- A pontuação na CNH funciona com o acréscimo de pontos na carteira de motorista a cada infração cometida. Caso o motorista atinja 40 pontos (em alguns casos), é instaurado um processo de suspensão da CNH.
+- Infração leve: 3 pontos
+- Infração média: 4 pontos
+- Infração grave: 5 pontos
+- Infração gravíssima: 7 pontos
+- Limite de 40 pontos - Só pode chegar nesta pontuação se não houver nenhuma infração gravíssima
+- Limite de 30 pontos - Só pode ter apenas uma infração gravíssima
+- Limite de 20 pontos - Se o condutor tiver duas ou mais infrações gravíssimas
+- Cada pontuação tem o prazo de validade de 1 ano a contar da data da infração. Após esse período, os pontos expiram.

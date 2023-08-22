533e9ae6baefced4b2e159205d91e7de82d4041b
Rafael <rafaelrocho_@hotmail.com>
2023-08-22 17:14:55 -0300

    v0.3 endpoints

--- actions/actions.py
+++ actions/actions.py
@@ -11,45 +11,54 @@ class ActionMultaGravidade(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        message = (
+            "Segue abaixo as informações sobre as multas:\n"
+            "Infração leve: R$ 88,38 - pontos na CNH: 3\n"
+            "Infração média: R$ 130,16 - pontos na CNH: 4\n"
+            "Infração grave: R$ 195,23 - pontos na CNH: 5\n"
+            "Infração gravíssima: R$ 293,47 - pontos na CNH: 7\n"
+            "OBS: Infração gravíssima possuem o fator multiplicador variando de acordo com a infração cometida, podendo ir de x1 até x20 o valor\n"
+        )
         
-        dispatcher.utter_message(text="utter_multa_gravidade")
+        dispatcher.utter_message(text=message)
         return []
 
-# informações sobre pontuação na CNH
-class ActionPontuacaoCNH(Action):
-    def name(self) -> Text:
-        return "action_pontuação_cnh"
+# # informações sobre pontuação na CNH
+# class ActionPontuacaoCNH(Action):
+#     def name(self) -> Text:
+#         return "action_pontuação_cnh"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-     # Enviando a resposta previamente definida no arquivo responses.md
-        dispatcher.utter_message(text="utter_pontuação_cnh")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#      # Enviando a resposta previamente definida no arquivo responses.md
+#         dispatcher.utter_message(text="utter_pontuação_cnh")
+#         return []
 
-# Artigos
+# # Artigos
 
-class ActionArt162ao164(Action):
-    def name(self) -> Text:
-        return "action_art_162_ao_164"
+# class ActionArt162ao164(Action):
+#     def name(self) -> Text:
+#         return "action_art_162_ao_164"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        dispatcher.utter_message(response="utter_art_162_ao_164")
-        return []
+#         dispatcher.utter_message(response="utter_art_162_ao_164")
+#         return []
 
-class ActionArt165AaoC(Action):
-    def name(self) -> Text:
-        return "action_art_165_A_ao_C"
+# class ActionArt165AaoC(Action):
+#     def name(self) -> Text:
+#         return "action_art_165_A_ao_C"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        dispatcher.utter_message(text="utter_art_165_A_ao_C")
-        return []
+#         dispatcher.utter_message(text="utter_art_165_A_ao_C")
+#         return []
 
 
 
